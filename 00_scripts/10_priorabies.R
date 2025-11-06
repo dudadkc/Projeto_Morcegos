2d89832be741c9618917a06be7d93931e55d96b5
Renata Muylaert <renatamuy@gmail.com>
2025-11-06 15:29:30 +1100

    prioritization maps

--- 00_scripts/10_priorabies.R
+++ 00_scripts/10_priorabies.R
@@ -1,10 +1,13 @@
 #' ----
-#' aim: prioritization
+#' aim: spatial prioritization rabies prevention
 #' author: re
 #' date: Nov 2025
 #' ----
 
 library(tidyverse)
+library(plotly)
+library(dplyr)
+library(viridis)
 library(sf)
 require(here)
 library(spdep)
@@ -22,6 +25,13 @@ library(grid)
 
 d <- st_read('C:/Users/rdel0062/Downloads/00_mun_data.gpkg')
 
+# Region data
+
+reg <- geobr::read_region(year = 2020) %>% 
+    sf::st_crop(xmin = -74, ymin = -34, xmax = -35, ymax = 5)
+reg
+
+# Data
 d$PRural <- as.numeric(d$PRural)
 
 df <- d %>% replace(is.na(.), 0) %>% as.data.frame()
@@ -169,10 +179,14 @@ png(filename = "venn_rabies_cases.png",
     height = 2000,
     res = 300       )
 
+
+venn_counts <- list( Human = which(human_cases), Feline = which(feline_cases), Canine = which(canine_cases) )
+
+
 venn.plot <- venn.diagram(
     x = venn_counts,
     filename = NULL,
-    fill = c("#FF9999", "#99CC99", "#6699CC"),
+    fill = c("salmon",'lightyellow' ,"skyblue"),
     alpha = 0.5,
     cex = 1.8,
     fontface = "bold",
@@ -180,7 +194,7 @@ venn.plot <- venn.diagram(
     cat.fontface = "bold",
     cat.pos = c(-20, 20, 180),
     cat.dist = c(0.05, 0.05, 0.05),
-    main = "Overlap of Rabies Cases",
+    main = "Overlap of rabies cases in Brazilian cities",
     main.cex = 2,
     main.fontface = "bold",
     lwd = 1.2
@@ -229,6 +243,7 @@ map_exposure <- ggplot(d) +
     theme_minimal()
 
 
+
 # all
 library(ggpubr)
 setwd(here())
@@ -241,6 +256,19 @@ ggarrange(
     map_exposure,
     ncol = 3,
     nrow = 1,
+    align = "v"
+)
+dev.off()
+
+
+
+jpeg(filename = "rabies_risk_components_long.jpg", width = 23, height = 30, units = "cm", res = 400)
+ggarrange(
+    map_hazard,
+    map_vulnerability,
+    map_exposure,
+    ncol = 1,
+    nrow = 3,
     align = "v"
 )
 dev.off()
@@ -306,7 +334,7 @@ label_coords <- d %>%
     slice_max(order_by = cases, n = 5) %>%  # top 10
     ungroup()
 
-# melt
+# melt 
 d_long <- d %>%
     pivot_longer(
         cols = c(RABIES_CASES_HUMAN_INFECTION,
@@ -323,11 +351,12 @@ d_long <- d %>%
     )
 
 
-fig_facets <- ggplot(d_long) +
-    geom_sf(aes(fill = cases), color = NA) +
+# Fig rabies in dogs, cats, and humans
+
+fig_facets1 <- ggplot(d_long) +
+    geom_sf(aes(fill = cases), colour = "gray80", linewidth = 0.01, alpha = 0.7) +
     geom_sf(data = reg, fill = NA, colour = "black", linewidth = 0.8) +
-    ggrepel::geom_text_repel(
-        data = label_coords,
+    ggrepel::geom_text_repel(      data = label_coords,
         aes(x = X, y = Y, label = paste0(NM_MUN, "\n(", cases, ")")),
         size = 3,
         box.padding = 0.5,
@@ -335,30 +364,30 @@ fig_facets <- ggplot(d_long) +
         segment.color = "gray"
     ) +
     scale_fill_gradient(
-        low = "purple1",
-        high = "darkgreen",
+        low = "darkgreen",
+        high = "purple1",
         na.value = "white",
         name = "Rabies cases"    ) +
     facet_wrap(~host, ncol = 1) +
     labs(
         title = "",
         fill = "Rabies cases",    x = "Longitude",           y = "Latitude"   ) +
-    theme_minimal() +
-    theme(
-        strip.text = element_text(face = "bold", size = 12),
+    annotation_north_arrow(pad_x = unit(1.2, "cm"), pad_y = unit(1.5, "cm"), which_north = "true", style = north_arrow_fancy_orienteering) +
+    annotation_scale(pad_x = unit(1.5, "cm"), pad_y = unit(1, "cm"), bar_cols = c("grey80", "white"), text_cex = 0.8) +
+    theme_minimal(base_family = "sans") +
+    theme( legend.position = "bottom",        strip.text = element_text(face = "bold", size = 12),
         plot.title = element_text(face = "bold", hjust = 0.5)
     )
 
-fig_facets
+fig_facets1
 
 setwd(here())
 setwd('99_manuscript')
-jpeg(filename = "rabies_historic_top10.jpg", width = 22, height = 32, units = "cm", res = 400)
-fig_facets
+jpeg(filename = "rabies_historic_top_long.jpg", width =22, height = 32, units = "cm", res = 400)
+fig_facets1
 dev.off()
 
-# Neighborhood
-
+# Neighborhood -----------------------------------------------------------------
 
 nb <- poly2nb(d) #slow
 
@@ -370,7 +399,6 @@ d[no_neighbors,]
 
 
 listw <- nb2listw(nb, style = "B", zero.policy = TRUE)  # binary weights, ignore islands
-
 
 
 d$historical_flag <- d_flagged$historical_flag
@@ -386,51 +414,37 @@ table(d$neighbor_risk)
 d <- d %>% mutate(  Spatial_prioritization_index = (exposure_average + hazard_average + vulnerability_average) / 3 + 
                  0.5 * historical_flag + 0.5 * neighbor_risk    )
 
-d <- d %>% mutate(  Spatial_prioritization_index = (exposure_average + hazard_average + vulnerability_average) / 3 + 
-                        0.5 * historical_flag + 0.5 * neighbor_risk )
+d <- d %>% mutate(  Spatial_prioritization_index_1 = (exposure_average + hazard_average + vulnerability_average) / 3 + 
+                       1 * historical_flag + 1 * neighbor_risk    )
+
 
 preds <- (d$exposure_average + d$hazard_average + d$vulnerability_average) / 3
 
 # Explo
 
 plot(preds, d$historical_flag)
-cor(d$exposure_average, d$historical_flag)
-cor(d$hazard_average, d$historical_flag) #negative???
-cor(d$vulnerability_average, d$historical_flag)
+cor(d$exposure_average, d$historical_flag) # positive
+cor(d$hazard_average, d$historical_flag) # negative
+cor(d$vulnerability_average, d$historical_flag) # positive
 
+cor(d$exposure_average, d$hazard_average)
+cor(d$exposure_average, d$vulnerability_average)
+cor(d$hazard_average, d$vulnerability_average)
 summary(d$CANINE_VACCINATED)
 
 cor(d$CANINE_VACCINATED, d$sdm_cont_mn)
 
-plot_ly(
-    data = d,
-    x = ~sdm_cont_mn,
-    y = ~CATTLE_POP,
-    z = ~CANINE_VACCINATED,
-    color = ~historical_flag, #+ RABIES_CASES_DOMESTIC_CANINE + RABIES_CASES_DOMESTIC_FELINE,
-    colors = viridis(100),
-    type = "scatter3d",
-    mode = "markers",
-    marker = list(size = 4, opacity = 0.6)
-)
-
-
-
-library(plotly)
-library(dplyr)
-library(viridis)
-
-plot_ly(
-    data = d,
-    x = ~exposure_average,
-    y = ~hazard_average,
-    z = ~vulnerability_average,
-    color = ~historical_flag, #+ RABIES_CASES_DOMESTIC_CANINE + RABIES_CASES_DOMESTIC_FELINE,
-    colors = viridis(100),
-    type = "scatter3d",
-    mode = "markers",
-    marker = list(size = 4, opacity = 0.6)
-)
+#plot_ly(
+ #   data = d,
+   # x = ~exposure_average,
+  #  y = ~hazard_average,
+  #  z = ~vulnerability_average,
+  #  color = ~historical_flag, #+ RABIES_CASES_DOMESTIC_CANINE + RABIES_CASES_DOMESTIC_FELINE,
+  #  colors = viridis(100),
+ #   type = "scatter3d",
+ #   mode = "markers",
+ #   marker = list(size = 4, opacity = 0.6)
+#)
 
 
 s check spatial correlation
@@ -447,6 +461,8 @@ moran.test(d$historical_flag, listw)
 
 moran.test(d$Spatial_prioritization_index, listw)
 
+moran.test(d$Spatial_prioritization_index_1, listw)
+
 hist(d$Spatial_prioritization_index)
 
 summary(d$Spatial_prioritization_index)
@@ -455,27 +471,53 @@ library(pROC)
 d$historical_flag
 roc_obj <- roc(d$historical_flag, d$Spatial_prioritization_index)
 
-auc(roc_obj)
-plot(roc_obj)
+roc_obj1 <- roc(d$historical_flag, d$Spatial_prioritization_index_1)
 
-# 
+auc(roc_obj)
+auc(roc_obj1)
+plot(roc_obj)
+plot(roc_obj1)
+# Classifying prioritization levels
+
+min(d$Spatial_prioritization_index)
+mean(d$Spatial_prioritization_index) + sd(d$Spatial_prioritization_index)
+max(d$Spatial_prioritization_index)
+
 d$scale_index <- scales::rescale(d$Spatial_prioritization_index)
 
+# n = n()
 
-quantile(d$scale_index)[1]
-#     0%        25%        50%        75%       100% 
 
-q <- quantile(d$scale_index, probs = c(0, 0.2, 0.4, 0.6, 0.8, 1))
+library(classInt)
+jenks_breaks <- classIntervals(d$Spatial_prioritization_index, n = 5, style = "jenks")$brks
+#Jenks Natural Breaks
+#groups similar values together while maximising the differences between groups
+
+jenks_breaks
+
+
+d %>%
+    filter(Spatial_prioritization_index > jenks_breaks[5]) %>%
+    select(name_muni, Spatial_prioritization_index)
+
+d %>%
+    filter(Spatial_prioritization_index > jenks_breaks[5]) %>%
+    select(name_muni, historical_flag, name_state)
+
+d_high_priority <- d %>%
+    filter(Spatial_prioritization_index > jenks_breaks[4]) %>%
+    select(name_muni, historical_flag, name_state)
+
 
 muni_sel <- d %>%
     mutate(
-        index = scale_index,
+        index = Spatial_prioritization_index,
         index_cat = case_when(
-            index <= q[2] ~ "Very low",
-            index > q[2] & index <= q[3] ~ "Low",
-            index > q[3] & index <= q[4] ~ "Average",
-            index > q[4] & index <= q[5] ~ "High",
-            index > q[5] ~ "Very high"
+            index <= jenks_breaks[2] ~ "Very low",
+            index > jenks_breaks[2] & index <= jenks_breaks[3] ~ "Low",
+            index > jenks_breaks[3] & index <= jenks_breaks[4] ~ "Average",
+            index > jenks_breaks[4] & index <= jenks_breaks[5] ~ "High",
+            index > jenks_breaks[5] ~ "Very high"
         ),
         index_cat = fct_relevel(index_cat, c("Very low", "Low", "Average", "High", "Very high"))
     )
@@ -484,16 +526,52 @@ quantile(muni_sel$scale_index)
 
 table(muni_sel$index_cat)
 
-sum(d$scale_index > 0.04151593, na.rm = TRUE)
+
+
+
+# Export table
+library(openxlsx)
+library(dplyr)
+
+# Arrange and select columns
+table(muni_sel$index_cat)
+out_table <- muni_sel %>%
+    arrange(factor(index_cat, levels = c("Very high", "High", "Average", "Low", "Very low"))) %>%
+    select(Municipality = name_muni,
+           State = name_state,
+           `Rabies detection` = historical_flag,
+           Priority = index_cat) %>%
+    st_set_geometry(NULL)
+
+nrow(out_table)
+
+write.xlsx(out_table, "Table_1_Rabies_Prioritization_Brazil_5571.xlsx", overwrite = TRUE)
+
+out_table_high <-  muni_sel %>%
+    filter(index_cat %in% c("Very high", "High")) %>%
+    arrange(factor(index_cat, levels = c("Very high", "High"))) %>%
+    select(Municipality = name_muni,
+           State = name_state,
+           `Rabies detection` = historical_flag,
+           Priority = index_cat) %>%
+    st_set_geometry(NULL)
+
+nrow(out_table_high)
+
+# Export to Excel
+#write.xlsx(out_table_high, "Table_1.xlsx", overwrite = TRUE)
+
+# Selectiion
 
 very_high_munis <- muni_sel %>%
-    filter(index_cat == "Very high" & historical_flag == 1) %>%
+    filter(index_cat == "Very high"| index_cat == "High" ) %>% #& historical_flag == 0
     pull(name_muni)
+
+very_high_munis
 
 
 length(very_high_munis)
 
-# Selectiion
 
 muni_cidades <- muni_sel %>% 
     dplyr::filter(name_muni %in% very_high_munis)
@@ -507,14 +585,11 @@ muni_cidades_coords <- muni_cidades %>%
         Y = st_coordinates(centroid)[,2]
     )
 
-muni_sel$index_cat
-
 map_prio <- ggplot() +   
     geom_sf(data = muni_sel, aes(fill = index_cat), colour = "white", linewidth = 0.01, alpha = 0.7) +
     # Region outline
-    geom_sf(data = reg, fill = NA, colour = "black", linewidth = 0.8) +
-    geom_text_repel(
-        data = st_centroid(muni_cidades_coords) %>%
+    geom_sf(data = reg, fill = NA, colour = "black", linewidth = 0.9) +
+    geom_text_repel(  data = st_centroid(muni_cidades_coords) %>%
             cbind(st_coordinates(.)),
         aes(x = X, y = Y, label = name_muni),
         colour = "white",
@@ -522,32 +597,25 @@ map_prio <- ggplot() +
         bg.r = 0.2,
         box.padding = 0.3,
         segment.colour = "grey60",
-        size = 3
-    ) +
-     scale_fill_brewer(palette = "RdYlGn", direction = -1, name = "Index") +
-     labs(
-        title = "Rabies spatial prioritization index & Historical rabies risk",
-        caption = "Font: IBGE"
-    ) +
-    annotation_north_arrow(
-        location = "br",
-        which_north = "true",
-        style = north_arrow_fancy_orienteering
-    ) +
-    annotation_scale(
-        location = "bl",
-        bar_cols = c("grey80", "white"),
-        text_cex = 0.8     ) +
+        size = 3, 
+        force = 2 ,
+        max.overlaps = 100) +
+    scale_fill_brewer(palette = "RdYlGn", direction = -1, name = "Index") +
+    labs(title = "", fill = "Spatial\nPrioritization Index") +
+    annotation_north_arrow(pad_x = unit(1.2, "cm"), pad_y = unit(1.5, "cm"), which_north = "true", style = north_arrow_fancy_orienteering) +
+    annotation_scale(pad_x = unit(1.5, "cm"), pad_y = unit(1, "cm"), bar_cols = c("grey80", "white"), text_cex = 0.8) +
     theme_minimal(base_family = "sans") +
-    theme(  panel.grid.major = element_blank(),
-            axis.text = element_blank(),
-            axis.ticks = element_blank(),
-            axis.title = element_blank(),
-            legend.position.inside = c(0.1, 0.3),
-            legend.background = element_blank(),
-            plot.title = element_text(face = "bold", size = 14, hjust = 0.5),
-            plot.caption = element_text(size = 9, hjust = 0, colour = "grey30"),
-            plot.background = element_rect(fill = "white", colour = NA) )
+    theme(panel.grid.major = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          axis.title = element_blank(),
+          legend.position = c(.15, .3),
+          legend.background = element_blank(),
+          legend.title = element_text(size = 15),
+          legend.text = element_text(size = 10),
+          plot.title = element_text(face = "bold", size = 14, hjust = 0.5),
+          plot.caption = element_text(size = 9, hjust = 0, colour = "grey30"),
+          plot.background = element_rect(fill = "white", colour = NA) )
 
 
 map_prio
@@ -555,105 +623,12 @@ map_prio
 # Export
 setwd(here())
 setwd('99_manuscript')
-ggsave( filename = "map_prio_AND_historical.jpg",
+ggsave( filename = "Figure_5.jpg",
         plot = map_prio,
         width = 10,
         height = 8,
         units = "in",
         dpi = 300,
         bg = "white" )
-
-
-# Just neighborhood
-
-very_high_munis <- muni_sel %>%
-    filter(index_cat == "Very high" & neighbor_risk== 1) %>%
-    pull(name_muni)
-
-length(very_high_munis)
-
-muni_cidades <- muni_sel %>% 
-    dplyr::filter(name_muni %in% very_high_munis)
-
-muni_cidades
-
-muni_cidades_coords <- muni_cidades %>%
-    mutate(centroid = st_centroid(.)) %>%
-    mutate(
-        X = st_coordinates(centroid)[,1],
-        Y = st_coordinates(centroid)[,2]
-    )
-
-muni_sel$index_cat
-
-map_prio_neighbor <- ggplot() +   
-    geom_sf(data = muni_sel, aes(fill = index_cat), colour = "white", linewidth = 0.01, alpha = 0.7) +
-    # Region outline
-    geom_sf(data = reg, fill = NA, colour = "black", linewidth = 0.8) +
-    # City points
-    #geom_sf(data = muni_sel, fill = "white", shape = 21, size = 2, colour = "gray40") +
-    geom_text_repel(
-        data = st_centroid(muni_cidades_coords) %>%
-            cbind(st_coordinates(.)),
-        aes(x = X, y = Y, label = name_muni),
-        colour = "white",
-        bg.color = "black",
-        bg.r = 0.2,
-        box.padding = 0.3,
-        segment.colour = "grey60",
-        size = 3
-    ) +
-    scale_fill_brewer(palette = "RdYlGn", direction = -1, name = "Index") +
-    labs(
-        title = "Rabies spatial prioritization index & Contiguity risk",
-        caption = "Font: IBGE"
-    ) +
-    annotation_north_arrow(
-        location = "br",
-        which_north = "true",
-        style = north_arrow_fancy_orienteering
-    ) +
-    annotation_scale(
-        location = "bl",
-        bar_cols = c("grey80", "white"),
-        text_cex = 0.8     ) +
-    theme_minimal(base_family = "sans") +
-    theme(  panel.grid.major = element_blank(),
-            axis.text = element_blank(),
-            axis.ticks = element_blank(),
-            axis.title = element_blank(),
-            legend.position.inside = c(0.1, 0.3),
-            legend.background = element_blank(),
-            plot.title = element_text(face = "bold", size = 14, hjust = 0.5),
-            plot.caption = element_text(size = 9, hjust = 0, colour = "grey30"),
-            plot.background = element_rect(fill = "white", colour = NA) )
-
-
-map_prio_neighbor
-
-# Export
-setwd(here())
-setwd('99_manuscript')
-ggsave( filename = "map_prio_AND_neihbor.jpg",
-        plot = map_prio_neighbor,
-        width = 10,
-        height = 8,
-        units = "in",
-        dpi = 300,
-        bg = "white" )
-
-
-#--------------
-
-jpeg(filename = "rabies_risk_components_long.jpg", width = 23, height = 30, units = "cm", res = 400)
-ggarrange(
-    map_hazard,
-    map_vulnerability,
-    map_exposure,
-    ncol = 1,
-    nrow = 3,
-    align = "v"
-)
-dev.off()
 
 #------------------------------------------------------------------------------------------
